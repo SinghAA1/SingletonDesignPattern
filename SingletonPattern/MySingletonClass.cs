6ba530a80badeee5beb5821fca14586eb51bc71f
SinghAA1 <A.A.Singh1@lse.ac.uk>
2018-01-20 01:18:06 +0000

    First

---
+++ SingletonPattern/MySingletonClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace SingletonPattern
+{
+    //there will only ever be one of these in existance per app domain
+    public class MySingletonClass : IDisposable
+    {
+        private bool _disposed;
+        //usage: var singleton = MySingletonClass.Instance
+        private static volatile MySingletonClass _instance;
+        private static readonly object _syncLock = new object();
+
+        public MySingletonClass()
+        {
+        }
+
+        public static MySingletonClass Instance
+        {
+            get
+            {
+                if(_instance != null) //already created
+                {
+                    return _instance;
+                }
+                lock(_syncLock) //single threaded
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new MySingletonClass();
+                    }
+                }
+                return _instance;
+               
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if(disposing)
+            {
+                _instance = null; //dispose managed instance
+            }
+            _disposed = true;
+                
+        }
+
+        ~MySingletonClass()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ SingletonPattern/MySingletonClassTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace SingletonPattern
+{
+    [TestFixture]
+    public class MySingletonClassTest
+    {
+        [Test]
+        public void TestSingleton()
+        {
+            var firstObject = MySingletonClass.Instance;
+            var secondobject = MySingletonClass.Instance;
+            Assert.AreSame(firstObject, secondobject); //pointing to same item on the heap
+            firstObject.Dispose();
+            secondobject.Dispose();
+        }
+    }
+
+}
